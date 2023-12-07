e194185476dfaa718476b939a2a5580e8f9761d7
Eugene Dmitrievskiy <y.dmytriievskyi@liga.net>
2023-12-07 13:28:00 +0200

    init dotfile configs

---
+++ nvim/lua/user/options.lua
@@ -0,0 +1,14 @@
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.title = true
+vim.opt.mouse = 'a' -- enable mouse for all modes
+vim.opt.termguicolors = true
+vim.opt.spell = true
