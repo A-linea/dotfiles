5e6552814bd40dd4a810d1d2774753452595f806
Eugene Dmitrievskiy <y.dmytriievskyi@liga.net>
2024-06-13 23:42:01 +0300

    added config files nvim

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -12,3 +12,19 @@ vim.opt.title = true
 vim.opt.mouse = 'a' -- enable mouse for all modes
 vim.opt.termguicolors = true
 vim.opt.spell = true
+vim.opt.wildmode = 'longest:full,full' -- complete the longest common match, and allow tabbing the results to fully complete them
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.fillchars:append({ eob = ' '})
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 10
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.confirm = true
+
+vim.opt.undofile = true
+vim.opt.backup = true
+vim.opt.backupdir:remove('.')
