63a7f8e9f09e4a5892b47ddcefd481704a4638b1
Eugene Dmitrievskiy <y.dmytriievskyi@liga.net>
2024-06-16 19:26:50 +0300

    fix copilot keymap

--- nvim/lua/eugene/plugins/copilot.lua
+++ nvim/lua/eugene/plugins/copilot.lua
@@ -4,7 +4,7 @@ return {
   'github/copilot.vim',
   config = function()
     vim.cmd([[
-      imap <silent><script><expr> <C-J> copilot#Accept("\<CR>")
+      imap <silent><script><expr> <C-h> copilot#Accept("\<CR>")
       let g:copilot_no_tab_map = v:true
     ]])
   end,
